4466660d04abd71143723d318f91fa367f986bfb
Max Eremin <admin@example.com>
2018-03-04 18:19:32 +0300

    weekly update

--- template-parts/content/content-posts.php
+++ template-parts/content/content-posts.php
@@ -5,69 +5,79 @@
         <span><?php echo __( 'Публикации', 'mais-awesome-theme' ) ?></span>
     </h1>
 
-    <?php 
-        $posts = array(
-            'posts_per_page' => 24,
-            'orderby' => 'rand'
-        );
+    <div class="posts-grid">
+        <?php 
+            $posts = array(
+                'posts_per_page' => 6,
+                'paged' => ( get_query_var( 'paged' ) 
+                    ? get_query_var( 'paged' ) 
+                    : 1 
+                )
+            );
 
-        foreach ( get_posts( $posts ) as $post ) :
-            setup_postdata( $post ); 
-    ?>
+            foreach ( get_posts( $posts ) as $post ) :
+                setup_postdata( $post ); 
+        ?>
 
-        <div class="flex__item">
-            
             <article <?php post_class( 'post' ); ?>>
 
                 <figure class="post__image">
                     <a href="<?php the_permalink(); ?>">
-                        <?php the_post_thumbnail( 'small', array(
+                        <?php the_post_thumbnail( array(300, 200), array(
                             'class' => 'post-image'
                         ) ); ?>
                     </a>
                 </figure>
 
-                <div class="post__date">
-                    <div class="red-line">
-                        <svg>
-                            <use xmlns:xlink="http://www.w3.org/1999/xlink" 
-                                xlink:href="<?php echo get_template_directory_uri() . "/assets/images/icons.svg#red-line"; ?>"></use>
-                        </svg>
+                <div class="flex-container">
+                    <div class="post__date">
+                        <div class="red-line">
+                            <svg>
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" 
+                                    xlink:href="<?php echo get_template_directory_uri() . "/assets/images/icons.svg#red-line"; ?>"></use>
+                            </svg>
+                        </div>
+                        <time datetime="<?php the_time( 'c' ) ?>"><?php the_time( 'd F Y H:i' ) ?></time>
+                        <div class="red-line">
+                            <svg>
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" 
+                                    xlink:href="<?php echo get_template_directory_uri() . "/assets/images/icons.svg#red-line"; ?>"></use>
+                            </svg>
+                        </div>
                     </div>
-                    <time datetime="<?php the_time( 'c' ) ?>"><?php the_time( 'd F Y H:i' ) ?></time>
-                    <div class="red-line">
-                        <svg>
-                            <use xmlns:xlink="http://www.w3.org/1999/xlink" 
-                                xlink:href="<?php echo get_template_directory_uri() . "/assets/images/icons.svg#red-line"; ?>"></use>
-                        </svg>
+
+                    <div class="post__title">
+                        <span><?php the_title(); ?></span>
                     </div>
-                </div>
 
-                <div class="post__title">
-                    <span><?php the_title(); ?></span>
-                </div>
+                    <div class="post__tags">
+                        <ul class="post__tags-list">
+                            <?php foreach ( wp_get_post_tags( $post->ID ) as $tag ) : ?>
+                                
+                                <li class="post__tag category_<?php echo $tag->slug; ?>">
+                                    <a href="<?php echo get_tag_link( $tag->term_id ); ?>">
+                                        <?php echo $tag->name; ?>
+                                    </a>
+                                </li>
 
-                <div class="post__tags">
-                    <ul class="post__tags-list">
-                        <?php foreach ( wp_get_post_tags( $post->ID ) as $tag ) : ?>
-                            
-                            <li class="post__tag category_<?php echo $tag->slug; ?>">
-                                <a href="<?php echo get_tag_link( $tag->term_id ); ?>">
-                                    <?php echo $tag->name; ?>
-                                </a>
-                            </li>
-
-                        <?php endforeach; ?>
-                    </ul>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
                 </div>
 
             </article>
 
-        </div>
+        <?php 
+            endforeach;
+            wp_reset_postdata();
+        ?>
 
-    <?php 
-        endforeach;
-        wp_reset_postdata();
-    ?>
+    </div>
+
+    <?php the_posts_pagination( array(
+        'mid_size' => 2,
+        'prev_text' => __( 'Назад', 'maisawesomeblog' ),
+        'next_text' => __( 'Вперед', 'maisawesomeblog' ),
+    ) ); ?>
 
 </section>
